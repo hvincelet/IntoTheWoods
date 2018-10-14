c8844cd633adbb72eb127e16e7cbaa9e49b9319c
SICARD <gsicard@enssat.fr>
2018-10-14 15:05:35 +0200

    adding routes /helper and /helper/id to register helper and redirect helper to default page

--- routes/helper.js
+++ routes/helper.js
@@ -2,6 +2,33 @@ const jdenticon = require('jdenticon');
 const pages_path = "../views/pages/";
 const models = require('../models');
 
+exports.displayRegister = function(req, res){
+
+    // TODO : get raid and activities informations from database and send it to ejs
+    models.raid.findAll({
+        attributes: ['id','name','edition','date','place']
+    }).then(function(get_raid) {
+
+        // TODO : get helper post  from database and send it to ejs
+        //models.helper_post.findAll({
+        //    attributes: ['id','id_point_of_interest','description']
+        //});
+
+        let get_raid_clean = [];
+        for(let raid in get_raid) {
+            get_raid_clean.push(get_raid[raid]['dataValues']);
+        }
+
+        if(get_raid_clean.length > 0) {
+            res.render(pages_path + "helper_register.ejs", {
+                pageTitle: "Inscription Bénévole",
+                raids: get_raid_clean
+          });
+        }
+
+    });
+};
+
 exports.register = function(req, res){
 
     let id = Math.random().toString(36).substr(2, 6); // generate id helper
@@ -30,15 +57,19 @@ exports.register = function(req, res){
                 first_name: req.body.registerUserFn
             }).then(function () {
                 // TODO: create assignment with poste(s) and helper in assignment table
-                
+
                 /*
                 models.assignment.create({ // create assignment with the new helper and poste(s)
-                    login: id,
-                    email: req.body.registerEmail,
-                    last_name: req.body.registerUserLn,
-                    first_name: req.body.registerUserFn
+                    id_helper: id,
+                    id_helper: ...
                 }).then(function () {
-                    // TODO:
+
+                    // TODO: redirect to helper_register with validMessage (organizer manage your inscription)
+
+                    //res.render(pages_path + "helper_register.ejs", {
+                    //    pageTitle: "Inscription Bénévole",
+                    //    valideMessage: "Organizer manage your inscription, you will be contact soon..."
+                    //});
 
                 });
                 */
@@ -61,6 +92,8 @@ exports.displayHome = function(req, res){
             console.log(helper_found.login);
             console.log(helper_found.last_name);
             console.log(helper_found.first_name);
+
+            // TODO : page to see the map with the path to go to helper post
 
         } else { // id of new helper does not exist
             res.render(pages_path + "helper_register.ejs", {
