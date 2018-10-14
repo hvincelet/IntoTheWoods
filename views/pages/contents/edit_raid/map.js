034658ba68eb99640cf5feb4f6c3a985f3074184
jderoux <derouxjulien@gmail.com>
2018-10-14 14:30:58 +0200

    Working on Get/Add of points of interest + Email View

--- views/pages/contents/edit_raid/map.js
+++ views/pages/contents/edit_raid/map.js
@@ -7,8 +7,10 @@ let raster = new ol.layer.Tile({
 });
 
 let source = new ol.source.Vector();
+
 let vector = new ol.layer.Vector({
     source: source,
+
     style: new ol.style.Style({
         fill: new ol.style.Fill({
             color: 'rgba(255, 255, 255, 0.2)'
@@ -52,9 +54,41 @@ function addInteractions() {
     map.addInteraction(draw);
     snap = new ol.interaction.Snap({source: source});
     map.addInteraction(snap);
-
 }
 
 function addPointOfInterest() {
     addInteractions();
 }
+
+function getVectorCoordonates() {
+    // Get the array of features
+    let features = vector.getSource().getFeatures();
+
+    // Go through this array and get coordinates of their geometry.
+    features.forEach(function (feature) {
+        console.log(ol.proj.toLonLat(feature.getGeometry().getCoordinates())); // /!\ retourne des projections != coord lonlat
+    });
+}
+
+function addVectorCoordonates() {
+
+    let pointsOfInterest = [[-3.4455227851867667, 48.818696208434886],
+        [-3.4416174888610835, 48.81996777291283],
+        [-3.437025547027587, 48.820363364171556]];
+
+    pointsOfInterest.forEach(function (pointsOfInterestCoords) {
+        let geom = new ol.geom.Point(ol.proj.fromLonLat(pointsOfInterestCoords));
+        let feature = new ol.Feature({
+                id: 'pointOfInterest',
+                geometry: geom,
+                popuptext: 'test'
+            }
+        );
+        source.addFeature(feature);
+    });
+
+}
+
+//https://openlayers.org/en/latest/examples/select-features.html
+//selectInteraction.getFeatures();
+
