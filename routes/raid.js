a257880ec9cddb7a42fbbf8715de3928c81cb98f
Gwendal Raballand <graballa@enssat.fr>
2018-11-07 14:18:29 +0100

    Bugs fixed and add modal for reseting password

--- routes/raid.js
+++ routes/raid.js
@@ -92,29 +92,34 @@ exports.displaySportsTable = function (req, res) {
 
 exports.saveSportsRanking = function (req, res) {
     let user = connected_user(req.sessionID);
-    JSON.parse(req.body.sports_list).forEach(function (sport_row) {
-
-        models.course.create({
-            order_num: sport_row.order,
-            label: sport_row.name,
-            id_sport: sport_row.sport,
-            id_raid: user.idCurrentRaid
-        }).then(function () {
-            models.raid.findByPk(user.idCurrentRaid)
-                .then(function (unique_raid_found) {
-                    user.raid_list.push({
-                        id: user.idCurrentRaid,
-                        name: unique_raid_found.dataValues.name,
-                        date: unique_raid_found.dataValues.date,
-                        edition: unique_raid_found.dataValues.edition,
-                        place: unique_raid_found.dataValues.place,
-                        lat: unique_raid_found.dataValues.lat,
-                        lng: unique_raid_found.dataValues.lng
-                    });
-                    return res.redirect('/editraid/' + user.idCurrentRaid + '/map');
-                });
+    const save_sports_actions = JSON.parse(req.body.sports_list).map(sport_row => {
+        return new Promise(resolve => {
+            console.log(sport_row);
+            models.course.create({
+                order_num: sport_row.order,
+                label: sport_row.name,
+                id_sport: sport_row.sport,
+                id_raid: user.idCurrentRaid
+            }).then(function () { return resolve(); });
         });
+    });
 
+    Promise.all(save_sports_actions).then(result => {
+        models.raid.findOne({
+            attributes: ['id', 'name', 'date', 'edition', 'place','lat','lng'],
+            where: {id: user.idCurrentRaid}
+        }).then(function(unique_raid_found){
+            user.raid_list.push({
+                id: user.idCurrentRaid,
+                name: unique_raid_found.dataValues.name,
+                date: unique_raid_found.dataValues.date,
+                edition: unique_raid_found.dataValues.edition,
+                place: unique_raid_found.dataValues.place,
+                lat: unique_raid_found.dataValues.lat,
+                lng: unique_raid_found.dataValues.lng
+            });
+            return res.redirect('/editraid/' + user.idCurrentRaid + '/map');
+        });
     });
 };
 
@@ -140,75 +145,21 @@ exports.getGeocodedResults = function (req, res) {
 
 exports.displayAllRaids = function (req, res) {
     const user = connected_user(req.sessionID);
-    if (user.raid_list.length !== 0) {
+    if(user.raid_list.length !== 0) {
         res.render(pages_path + "template.ejs", {
             pageTitle: "Gestion des Raids",
             page: "edit_raid/all",
             user: user
         });
-    } else {
+    }else{
         res.redirect('/dashboard');
     }
 };
 
-/*exports.displayRaid = function (req, res) {
+exports.displayRaid = function(req, res) {
     const user = connected_user(req.sessionID);
-    if(user.raid_list.length === 0) {
-        res.redirect('/dashboard');
-    }else{
-        let found = user.raid_list.find(function (raid) {
-            return raid.id === parseInt(req.params.id);
-        });
-        if(!found){
-            res.redirect('/editraid')
-        }else{ // User is authenticated and allow to access this page
-            let organizers_linked_with_the_current_raid = [];
-            let helpers_linked_with_the_current_raid = [];
-             // get organizer (email, first_name, last_name)
-            organizers_linked_with_the_current_raid.push({
-                email: "graballa@enssat.fr",
-                first_name: 'Gwendal',
-                last_name: 'Raballand'
-            });
-            organizers_linked_with_the_current_raid.push({
-                email: "jderoux@enssat.fr",
-                first_name: 'Julien',
-                last_name: 'Deroux'
-            });
-
-            // Get helpers (email, first_name, last_name, posts)
-            helpers_linked_with_the_current_raid.push({
-               email: 'hvincele@enssat.fr',
-               first_name: 'Hugo',
-               last_name: 'Vincelet',
-               posts: ['Accueil', 'Buvette', 'Kayak', 'Circulation']
-            })
-            helpers_linked_with_the_current_raid.push({
-                email: 'gsicard@enssat.fr',
-                first_name: 'Guillaume',
-                last_name: 'Sicard',
-                posts: ['Circulation']
-            });
-
-            // Get Courses
-
-            res.render(pages_path + "template.ejs", {
-                pageTitle: "Gestion d'un Raid",
-                page: "edit_raid/details",
-                user: user,
-                raid: found,
-                organizers: organizers_linked_with_the_current_raid,
-                helpers: helpers_linked_with_the_current_raid
-            });
-        }
-    }*/
-
-exports.displayRaid = function (req, res) {
-    const user = connected_user(req.sessionID);
-    const raid = user.raid_list.find(function (raid) {
-        return raid.id == req.params.id
-    });
-    if (!raid) {
+    const raid = user.raid_list.find(function(raid){return raid.id == req.params.id});
+    if(!raid){
         return res.redirect('/dashboard');
     }
 
@@ -229,10 +180,10 @@ exports.displayRaid = function (req, res) {
         where: {
             id_raid: req.params.id
         }
-    }).then(function (organizers_found) {
+    }).then(function(organizers_found){
 
         let organizers_linked_with_the_current_raid = [];
-        organizers_found.forEach(function (organizer) {
+        organizers_found.forEach(function(organizer){
             organizers_linked_with_the_current_raid.push({
                 email: organizer.dataValues.organizer.dataValues.email,
                 first_name: organizer.dataValues.organizer.dataValues.first_name,
@@ -251,7 +202,6 @@ exports.displayRaid = function (req, res) {
         let helper_post_model = models.helper_post;
         let point_of_interest_model = models.point_of_interest;
 
-        //helper_model.belongsTo(assignment_model, {foreignKey: 'login'});
         assignment_model.belongsTo(helper_model, {foreignKey: 'id_helper'});
         assignment_model.belongsTo(helper_post_model, {foreignKey: 'id_helper_post'});
         helper_post_model.belongsTo(point_of_interest_model, {foreignKey: 'id_point_of_interest'});
@@ -269,15 +219,17 @@ exports.displayRaid = function (req, res) {
                 }]
             }]
         }).then(function (assignment_found) {
-
             const unique_assignments_array = assignment_found.filter(function (assignment, index, array) {
                 return array.findIndex(function (value) {
-                    return value.dataValues.id_helper == assignment.dataValues.id_helper;
-                }) == index;
+                    return value.dataValues.id_helper === assignment.dataValues.id_helper;
+                }) === index;
             });
 
             const storeHelperActions = unique_assignments_array.map((assignment, index) => {
                 return new Promise((resolve, reject) => {
+                    if(assignment.dataValues.helper_post === null){
+                        return resolve();
+                    }
                     helper_model.findOne({
                         attributes: ['login', 'email', 'last_name', 'first_name'],
                         where: {
@@ -302,7 +254,7 @@ exports.displayRaid = function (req, res) {
                             attributed: attributed,
                             assignment: []
                         };
-                        assignments_by_id_helper.forEach(function (assignment_by_id_helper) {
+                        assignments_by_id_helper.forEach(function(assignment_by_id_helper){
                             helper.assignment.push({
                                 id: assignment_by_id_helper.dataValues.id_helper_post,
                                 description: assignment_by_id_helper.dataValues.helper_post.dataValues.description
@@ -326,60 +278,38 @@ exports.displayRaid = function (req, res) {
                 let course_model = models.course;
                 let sport_model = models.sport;
 
-                sport_model.belongsTo(course_model, {foreignKey: 'id'});
-                const Sequelize = require('sequelize');
-                sport_model.findAll({
-                    attributes: ['name'],
+                course_model.belongsTo(sport_model, {foreignKey: 'id_sport'});
+
+                course_model.findAll({
+                    attributes: ['order_num'],
                     include: [{
-                        model: course_model,
-                        attributes: ['order_num'],
-                        where: {
-                            id_sport: Sequelize.col('sport.id'),
-                            id_raid: req.params.id
-                        }
-                    }]
-                }).then(function (course_name_and_order_found) {
-                    course_name_and_order_found.forEach(function (course) {
+                        model: sport_model,
+                        attributes: ['name']
+                    }],
+                    where: {
+                        id_raid: req.params.id
+                    }
+                }).then(function(course_name_and_order_found){
+
+                    course_name_and_order_found.map(function(course){
                         courses_linked_with_the_current_raid.push({
-                            order: course.dataValues.course.order_num,
-                            name: course.dataValues.name
+                            order: course.dataValues.order_num,
+                            name: course.dataValues.sport.dataValues.name
                         });
                     });
-
-
-                    /******************/
-                    /*      STUB      */ // TODO : Change details.ejs to handle data_helper
-                    /******************/
-
-                    let helpers_linked_with_the_current_raid = [];
-                    helpers_linked_with_the_current_raid.push({
-                        email: 'hvincele@enssat.fr',
-                        first_name: 'Hugo',
-                        last_name: 'Vincelet',
-                        posts: ['Accueil', 'Buvette', 'Kayak', 'Circulation']
-                    })
-                    helpers_linked_with_the_current_raid.push({
-                        email: 'gsicard@enssat.fr',
-                        first_name: 'Guillaume',
-                        last_name: 'Sicard',
-                        posts: ['Circulation']
-                    });
-
 
                     res.render(pages_path + "template.ejs", {
                         pageTitle: "Gestion d'un Raid",
                         page: "edit_raid/details",
                         user: user,
                         organizers: organizers_linked_with_the_current_raid,
-                        //helpers: data_helper,
-                        helpers: helpers_linked_with_the_current_raid,
+                        helpers: data_helper,
                         courses: courses_linked_with_the_current_raid,
                         raid: raid
                     });
 
                 });
             });
-
         });
 
     });
