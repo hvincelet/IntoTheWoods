b2b08687740e9b7f0c2a3c6cdff539dafd88dfca
Gwendal Raballand <graballa@enssat.fr>
2018-11-07 09:00:54 +0100

    Bug fixed

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ module.exports = {
         database: 'intoTheWoodsDB',
         host: 'localhost',
         dialect: 'mysql',
-        logging: false, // True for logging sequelize message
-        no_login: false,
+        logging: false, // true for logging sequelize message
+        no_login: true,
         define: {
             timestamps: false
         }
