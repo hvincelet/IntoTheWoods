9f1765a4195588f11dac37e08210f794e8e841ba
Gwendal Raballand <graballa@enssat.fr>
2018-11-08 14:37:35 +0100

    Add mail functions

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
         database: 'intoTheWoodsDB',
         host: 'localhost',
         dialect: 'mysql',
-        logging: false, // true for logging sequelize message
+        logging: true, // true for logging sequelize message
         no_login: false, // true for being authenticated
         define: {
             timestamps: false
