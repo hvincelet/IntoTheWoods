d2d715d234d3a3ff27387d692a281f7f6c8b283f
Unknown <simon.bonnaud@gmail.com>
2018-12-12 12:07:43 +0100

    Fixed the bug on registering

--- routes/participant.js
+++ routes/participant.js
@@ -4,7 +4,7 @@ const models = require('../models');
 
 // Register new participant default page
 exports.displayRegister = function(req, res){
-    let get_raids_clean = [];
+    var  get_raids_clean = [];
 
     let raid_model = models.raid;
 
@@ -12,26 +12,32 @@ exports.displayRegister = function(req, res){
       attributes: ['id','name','date','edition']
     }).then(function(raids_found){
         if(raids_found !== null){
-            raids_found.forEach(function(raid, index, raid_array){
-                models.raid.findAndCountAll().then(function(all_assignement){
-                    if(Date.parse(raid.dataValues.date) >= Date.now()){
-                        get_raids_clean.push({'id':raid.dataValues.id,'name':raid.dataValues.name,'edition':raid.dataValues.edition,'date':raid.dataValues.date});
-                    }
-                });
+            const raidsCounter = raids_found.map(function(raid, index, raid_array){
+                return new Promise(resolve => {
+                    models.raid.findAndCountAll().then(function(all_assignement){
+                        if(Date.parse(raid.dataValues.date) >= Date.now()){
+                            get_raids_clean.push({'id':raid.dataValues.id,'name':raid.dataValues.name,'edition':raid.dataValues.edition,'date':raid.dataValues.date});
+                        }
+                        return resolve();
+                    });
+                })
             });
 
-            if(get_raids_clean !== null){
-                res.render(pages_path + "participant_register.ejs", {
-                    pageTitle: "Inscription Participant",
-                    raids: get_raids_clean
-                });
-            }
-            else{
-                res.render(pages_path + "participant_register.ejs", {
-                    pageTitle: "Inscription Participant",
-                    errorMessage: "Aucun raid à venir pour le moment. Veuillez réessayer plus tard."
-                });
-            }
+
+            Promise.all(raidsCounter).then(function(){
+                if(get_raids_clean !== null && get_raids_clean.length > 0){
+                    res.render(pages_path + "participant_register.ejs", {
+                        pageTitle: "Inscription Participant",
+                        raids: get_raids_clean
+                    });
+                }
+                else{
+                    res.render(pages_path + "participant_register.ejs", {
+                        pageTitle: "Inscription Participant",
+                        errorMessage: "Aucun raid à venir pour le moment. Veuillez réessayer plus tard."
+                    });
+                }
+            })
         }
         else{
             res.render(pages_path + "participant_register.ejs", {
